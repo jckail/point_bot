f0075c6f81caeda7342109a0bafaa24f0ae74086
jckail <jckail13@gmail.com>
2020-06-08 12:48:12 -0600

    modified runner.sh only effects linux

--- point_bot/runner.sh
+++ point_bot/runner.sh
@@ -3,7 +3,6 @@
 echo "Executing Nord VPN"
 nordvpn connect us5215
 echo "Executing Python"
-#python3.8 /home/ubuntu/point_bot/point_bot/main.py
-python3.8 /Users/jckail13/pointly/point_bot/point_bot/main.py
+python3.8 /home/ubuntu/point_bot/point_bot/main.py
 echo "Disconnecting VPN"
 nordvpn disconnect
